c42c0ebdc349d6f818c19448bdb8e11a187c8001
KOMARA CHANDRA PRASAD <komarachandraprasad@gmail.com>
2023-08-29 22:06:59 +0530

    lab1start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import Home1 from '../Lab1/Home1';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <h1>khgfugrk</h1>
-    </>
+    <Router>
+      <Link to="/lab1" >Lab 1</Link>
+      <br></br>
+      <Link to="/lab2" >Lab 2</Link>
+      <br></br>
+      <Link to="/lab3" >Lab 3</Link>
+      <br></br>
+      <Link to="/lab4" >Lab 4</Link>
+      <br></br>
+      <Link to="/lab5" >Lab 5</Link>
+      <br></br>
+      <Link to="/lab6&7" >Lab 6&7</Link>
+
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/lab1" element={<Home1 />} />
+      </Routes>
+    </Router>
   );
 }
 
