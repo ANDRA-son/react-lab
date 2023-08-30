90d575a573a69cafc227548e37d3450f5f32d353
KOMARA CHANDRA PRASAD <komarachandraprasad@gmail.com>
2023-08-30 12:45:50 +0530

    hol

--- src/Lab1/Home1.js
+++ src/Lab1/Home1.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Registeration from './Regestration'
 
 export default function Home1() {
   return (
     <>
-      Home
+      <Registeration/>
     </>
   )
 }
--- src/Lab1/Regestration.js
+++ src/Lab1/Regestration.js
@@ -1,26 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Registeration() {
-    let [user, setUser] = useState({ username: '', password: '' })
-    let { username, password } = user;
-    let handleChange = (e) => {
-        let { name, value } = e.target
-        setUser({ ...user, username: 'trisha' })//username:'revathi',password:''
-        //setUser({..user,age:44});
-        // username,password,age
-    }
-    let handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(user);
-    }
-    return (
-        <form onSubmit={handleSubmit}>
+  let [user, setUser] = useState({ username: "", email: "", password: "" });
+  //let { username, password } = user;
 
-            Username:<input type="text" name="username" onChange={handleChange} /><br />
-            Password:<input type="text" name="password" onChange={handleChange} /><br />
-            <button type="submit">Submit</button>
-        </form>
-    )
+  let emailValidator = (email) => {
+    if (!email) {
+      return "Email is required";
+    } else if (!new RegExp(/\S+@\S+\.\S+/).test(email)) {
+      return "Incorrect email format";
+    }
+    return "";
+  };
+
+  let passwordValidator = (password) => {
+    if (!password) {
+      return "Password is required";
+    } else if (password.length < 8) {
+      return "Password must have a minimum 8 characters";
+    }
+    return "";
+  };
+
+  let confirmPasswordValidator = (confirmPassword, form) => {
+    if (!confirmPassword) {
+      return "Confirm password is required";
+    } else if (confirmPassword.length < 8) {
+      return "Confirm password must have a minimum 8 characters";
+    } else if (confirmPassword !== user.password) {
+      return "Passwords do not match";
+    }
+    return "";
+  };
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  let handleSubmit = (e) => {
+    console.log(user);
+  };
+  return (
+    <>
+      {user.username}
+      <br></br>
+      Username:
+      <input type="text" name="username" onChange={handleChange} />
+      <br />
+      Email:
+      <input type="text" name="email" onChange={handleChange} />
+      <br />
+      Password:
+      <input type="text" name="password" onChange={handleChange} />
+      <br />
+      Confirm Password:
+      <input type="text" name="password" onChange={handleChange} />
+      <br />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </>
+  );
 }
 
-export default Registeration
+export default Registeration;
